6f73f4056783c5010133942ee84258cdfdd51bf8
sebastianG <sebastian_gresala@yahoo.com>
2025-03-22 01:04:53 +0200

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -3,37 +3,37 @@ resource "azurerm_resource_group" "weather_app_rg" {
   location = var.weather_app_resource_group_location
 }
 
-resource "azurerm_virtual_network" "my_vnet" {
+resource "azurerm_virtual_network" "weather_app_vnet" {
   name                = "weather-app-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.weather_app_rg.location
   resource_group_name = azurerm_resource_group.weather_app_rg.name
 }
 
-resource "azurerm_subnet" "my_terraform_subnet" {
+resource "azurerm_subnet" "weather_app_subnet" {
   name                 = "weather-app-subnet"
   resource_group_name  = azurerm_resource_group.weather_app_rg.name
-  virtual_network_name = azurerm_virtual_network.my_vnet.name
+  virtual_network_name = azurerm_virtual_network.weather_app_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "my_terraform_public_ip" {
+resource "azurerm_public_ip" "weather_app_public_ip" {
   name                = "weather-app-public-ip"
   resource_group_name = azurerm_resource_group.weather_app_rg.name
   location            = azurerm_resource_group.weather_app_rg.location
   allocation_method   = "Dynamic"
 }
 
-resource "azurerm_network_interface" "my_terraform_nic" {
+resource "azurerm_network_interface" "weather_app_nic" {
   name                = "weather-app-nic"
   location            = azurerm_resource_group.weather_app_rg.location
   resource_group_name = azurerm_resource_group.weather_app_rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
+    subnet_id                     = azurerm_subnet.weather_app_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
+    public_ip_address_id          = azurerm_public_ip.weather_app_public_ip.id
   }
 }
 
@@ -41,7 +41,7 @@ resource "azurerm_linux_virtual_machine" "k3s_vm" {
   name                  = "k3svm"
   location              = azurerm_resource_group.weather_app_rg.location
   resource_group_name   = azurerm_resource_group.weather_app_rg.name
-  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
+  network_interface_ids = [azurerm_network_interface.weather_app_nic.id]
   size                  = "Standard_DS1_v2"
 
   os_disk {
@@ -70,7 +70,7 @@ resource "azurerm_linux_virtual_machine" "observability_vm" {
   name                  = "observabilityvm"
   location              = azurerm_resource_group.weather_app_rg.location
   resource_group_name   = azurerm_resource_group.weather_app_rg.name
-  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
+  network_interface_ids = [azurerm_network_interface.weather_app_nic.id]
   size                  = "Standard_DS1_v2"
 
   os_disk {
