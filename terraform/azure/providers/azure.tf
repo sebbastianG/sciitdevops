5522a6ae46c454ff7b289e4354c5d10ecff8c29c
sebastianG <sebastian_gresala@yahoo.com>
2025-03-22 00:09:41 +0200

    Update azure.tf
    updated tf azure code for deployment

--- terraform/azure/providers/azure.tf
+++ terraform/azure/providers/azure.tf
@@ -2,12 +2,16 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = ">=3.0.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-}
 
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+}
