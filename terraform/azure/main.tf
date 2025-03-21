1f148152044fa48fc8516905babdd374c1782434
sebastianG <sebastian_gresala@yahoo.com>
2025-03-22 00:50:50 +0200

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,10 +1,8 @@
-# Create Resource Group for the Weather App
 resource "azurerm_resource_group" "weather_app_rg" {
   name     = var.weather_app_resource_group_name
   location = var.weather_app_resource_group_location
 }
 
-# Create Virtual Network
 resource "azurerm_virtual_network" "my_vnet" {
   name                = "weather-app-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -12,7 +10,6 @@ resource "azurerm_virtual_network" "my_vnet" {
   resource_group_name = azurerm_resource_group.weather_app_rg.name
 }
 
-# Create Subnet
 resource "azurerm_subnet" "my_terraform_subnet" {
   name                 = "weather-app-subnet"
   resource_group_name  = azurerm_resource_group.weather_app_rg.name
@@ -20,7 +17,6 @@ resource "azurerm_subnet" "my_terraform_subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Create Public IP
 resource "azurerm_public_ip" "my_terraform_public_ip" {
   name                = "weather-app-public-ip"
   resource_group_name = azurerm_resource_group.weather_app_rg.name
@@ -28,7 +24,6 @@ resource "azurerm_public_ip" "my_terraform_public_ip" {
   allocation_method   = "Dynamic"
 }
 
-# Create Virtual Machine for K3s
 resource "azurerm_linux_virtual_machine" "k3s_vm" {
   name                  = "k3svm"
   location              = azurerm_resource_group.weather_app_rg.location
@@ -58,7 +53,6 @@ resource "azurerm_linux_virtual_machine" "k3s_vm" {
   }
 }
 
-# Create Virtual Machine for Observability
 resource "azurerm_linux_virtual_machine" "observability_vm" {
   name                  = "observabilityvm"
   location              = azurerm_resource_group.weather_app_rg.location
