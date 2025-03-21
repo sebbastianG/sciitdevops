0f49bbac10709cd86b6e266899921f2c3e325d54
sebastianG <sebastian_gresala@yahoo.com>
2025-03-22 01:05:20 +0200

    Update security.tf

--- terraform/azure/security.tf
+++ terraform/azure/security.tf
@@ -1,5 +1,5 @@
-resource "azurerm_network_security_group" "my_terraform_nsg" {
-  name                = "myNetworkSecurityGroup"
+resource "azurerm_network_security_group" "weather_app_nsg" {
+  name                = "weather-app-nsg"
   location            = azurerm_resource_group.weather_app_rg.location
   resource_group_name = azurerm_resource_group.weather_app_rg.name
 
@@ -16,39 +16,7 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
   }
 }
 
-resource "azurerm_network_interface" "my_terraform_nic" {
-  name                = "myNIC"
-  location            = azurerm_resource_group.weather_app_rg.location
-  resource_group_name = azurerm_resource_group.weather_app_rg.name
-
-  ip_configuration {
-    name                          = "my_nic_configuration"
-    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
-  }
+resource "azurerm_network_interface_security_group_association" "weather_app_nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.weather_app_nic.id
+  network_security_group_id = azurerm_network_security_group.weather_app_nsg.id
 }
-
-resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
-  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
-}
-
-# Generate random text for a unique storage account name
-resource "random_id" "random_id" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group = azurerm_resource_group.weather_app_rg.name
-  }
-
-  byte_length = 8
-}
-
-# Create storage account for boot diagnostics (Now Removed)
-# resource "azurerm_storage_account" "my_storage_account" {
-#   name                     = "diag${random_id.random_id.hex}"
-#   location                 = azurerm_resource_group.weather_app_rg.location
-#   resource_group_name      = azurerm_resource_group.weather_app_rg.name
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
