609dde92e0a8499b3cbb14502d4d7e2e46307079
sebastianG <sebastian_gresala@yahoo.com>
2025-03-22 00:12:38 +0200

    Update ssh.tf

--- terraform/azure/ssh.tf
+++ terraform/azure/ssh.tf
@@ -1,24 +1,8 @@
-resource "random_pet" "ssh_key_name" {
-  prefix    = "ssh"
-  separator = ""
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
-resource "azapi_resource_action" "ssh_public_key_gen" {
-  type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  resource_id = azapi_resource.ssh_public_key.id
-  action      = "generateKeyPair"
-  method      = "POST"
-
-  response_export_values = ["publicKey", "privateKey"]
-}
-
-resource "azapi_resource" "ssh_public_key" {
-  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  name      = random_pet.ssh_key_name.id
-  location  = azurerm_resource_group.rg.location
-  parent_id = azurerm_resource_group.rg.id
-}
-
-output "key_data" {
-  value = azapi_resource_action.ssh_public_key_gen.output.publicKey
+output "ssh_public_key" {
+  value = tls_private_key.ssh.public_key_openssh
 }
