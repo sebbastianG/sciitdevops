6a872d2cedebdb456b39f65d21f8dc09ec70dc42
sebastianG <sebastian_gresala@yahoo.com>
2025-03-22 14:03:36 +0200

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -5,28 +5,33 @@ data "azurerm_resource_group" "weather_app_rg" {
 resource "azurerm_virtual_network" "weather_app_vnet" {
   name                = "weather-app-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.weather_app_rg.location
-  resource_group_name = azurerm_resource_group.weather_app_rg.name
+  location            = data.azurerm_resource_group.weather_app_rg.location
+  resource_group_name = data.azurerm_resource_group.weather_app_rg.name
 }
 
 resource "azurerm_subnet" "weather_app_subnet" {
   name                 = "weather-app-subnet"
-  resource_group_name  = azurerm_resource_group.weather_app_rg.name
+  resource_group_name  = data.azurerm_resource_group.weather_app_rg.name
   virtual_network_name = azurerm_virtual_network.weather_app_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "weather_app_public_ip" {
   name                = "weather-app-public-ip"
-  resource_group_name = azurerm_resource_group.weather_app_rg.name
-  location            = azurerm_resource_group.weather_app_rg.location
+  resource_group_name = data.azurerm_resource_group.weather_app_rg.name
+  location            = data.azurerm_resource_group.weather_app_rg.location
   allocation_method   = "Dynamic"
+  sku                 = "Standard"
+  sku_tier            = "Regional"
+  ddos_protection_mode = "VirtualNetworkInherited"
+  idle_timeout_in_minutes = 4
+  ip_version          = "IPv4"
 }
 
 resource "azurerm_network_interface" "weather_app_nic" {
   name                = "weather-app-nic"
-  location            = azurerm_resource_group.weather_app_rg.location
-  resource_group_name = azurerm_resource_group.weather_app_rg.name
+  location            = data.azurerm_resource_group.weather_app_rg.location
+  resource_group_name = data.azurerm_resource_group.weather_app_rg.name
 
   ip_configuration {
     name                          = "internal"
