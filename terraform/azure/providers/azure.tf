668384d3ceee23862dba2171267ea0c9b2c82afd
sebastianG <sebastian_gresala@yahoo.com>
2025-03-22 13:36:52 +0200

    Update azure.tf

--- terraform/azure/providers/azure.tf
+++ terraform/azure/providers/azure.tf
@@ -1,19 +1,1 @@
-# Configure Terraform to use the AzureRM provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"  # Lock to a suitable version (example)
-    }
-  }
-}
 
-# AzureRM provider configuration (using variables for sensitive values)
-provider "azurerm" {
-  features {}  # Required empty block for Azure provider defaults
-
-  subscription_id = var.azure_subscription_id
-  tenant_id       = var.azure_tenant_id
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-}
