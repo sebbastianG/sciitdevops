ee3436c29061d6a5eec9436df205850c0225fed2
sebastianG <sebastian_gresala@yahoo.com>
2025-03-22 17:49:34 +0200

    Update variables.tf

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -48,10 +48,4 @@ output "ssh_public_key" {
   value = tls_private_key.ssh.public_key_openssh
 }
 
-# -------------------------
-# ssh.tf
-# -------------------------
-resource "tls_private_key" "ssh" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
+
