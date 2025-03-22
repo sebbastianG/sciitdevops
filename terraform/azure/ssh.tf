dac2d9fcdf1049ed87125c85316d4c28e993373e
sebastianG <sebastian_gresala@yahoo.com>
2025-03-22 17:02:19 +0200

    Update ssh.tf

--- terraform/azure/ssh.tf
+++ terraform/azure/ssh.tf
@@ -1,8 +1,11 @@
+# ssh.tf
 resource "tls_private_key" "ssh" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
+
+# outputs.tf
 output "ssh_public_key" {
   value = tls_private_key.ssh.public_key_openssh
 }
