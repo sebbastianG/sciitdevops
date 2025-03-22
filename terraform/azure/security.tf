8af37d919ace96a87e07703eb804ccbc73c03350
sebastianG <sebastian_gresala@yahoo.com>
2025-03-22 17:02:10 +0200

    Update security.tf

--- terraform/azure/security.tf
+++ terraform/azure/security.tf
@@ -1,5 +1,6 @@
+# security.tf
 resource "azurerm_network_security_group" "weather_app_nsg" {
-  name                = "weather-app-nsg"
+  name                = "${var.resource_group_name}-nsg"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
 
