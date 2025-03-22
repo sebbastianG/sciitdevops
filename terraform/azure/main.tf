87091309534efb89eda6e0e78f44ea2d498d60c7
sebastianG <sebastian_gresala@yahoo.com>
2025-03-22 17:01:57 +0200

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,12 +1,4 @@
 # main.tf
-
-# Removed azurerm_resource_group since it's managed externally
-
-resource "tls_private_key" "ssh" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
 resource "azurerm_virtual_network" "main" {
   name                = "${var.resource_group_name}-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -25,33 +17,15 @@ resource "azurerm_subnet" "main" {
 }
 
 resource "azurerm_public_ip" "main" {
-  name                = "${var.resource_group_name}-public-ip"
-  location            = var.resource_group_location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Dynamic"
-  sku                 = "Basic"
-  sku_tier            = "Regional"
-  ddos_protection_mode = "VirtualNetworkInherited"
+  name                    = "${var.resource_group_name}-public-ip"
+  location                = var.resource_group_location
+  resource_group_name     = var.resource_group_name
+  allocation_method       = "Dynamic"
+  sku                     = "Basic"
+  sku_tier                = "Regional"
+  ddos_protection_mode    = "VirtualNetworkInherited"
   idle_timeout_in_minutes = 4
-  ip_version = "IPv4"
-}
-
-resource "azurerm_network_security_group" "weather_app_nsg" {
-  name                = "${var.resource_group_name}-nsg"
-  location            = var.resource_group_location
-  resource_group_name = var.resource_group_name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+  ip_version              = "IPv4"
 }
 
 resource "azurerm_network_interface" "main" {
@@ -66,9 +40,4 @@ resource "azurerm_network_interface" "main" {
     private_ip_address_version    = "IPv4"
     public_ip_address_id          = azurerm_public_ip.main.id
   }
-}
-
-resource "azurerm_network_interface_security_group_association" "weather_app_nsg_assoc" {
-  network_interface_id      = azurerm_network_interface.main.id
-  network_security_group_id = azurerm_network_security_group.weather_app_nsg.id
 }
