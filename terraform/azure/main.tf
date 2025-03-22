d864ae279d9708922c9f4d290189a340211434e8
sebastianG <sebastian_gresala@yahoo.com>
2025-03-22 16:56:19 +0200

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,30 +1,45 @@
-resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
+# main.tf
+
+# Removed azurerm_resource_group since it's managed externally
+
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
 resource "azurerm_virtual_network" "main" {
   name                = "${var.resource_group_name}-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  private_endpoint_vnet_policies = "Disabled"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_subnet" "main" {
-  name                 = "${var.resource_group_name}-subnet"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.1.0/24"]
+  name                                          = "${var.resource_group_name}-subnet"
+  resource_group_name                           = var.resource_group_name
+  virtual_network_name                          = azurerm_virtual_network.main.name
+  address_prefixes                              = ["10.0.1.0/24"]
   private_endpoint_network_policies             = "Disabled"
   private_link_service_network_policies_enabled = true
   default_outbound_access_enabled               = true
 }
 
+resource "azurerm_public_ip" "main" {
+  name                = "${var.resource_group_name}-public-ip"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+  sku_tier            = "Regional"
+  ddos_protection_mode = "VirtualNetworkInherited"
+  idle_timeout_in_minutes = 4
+  ip_version = "IPv4"
+}
+
 resource "azurerm_network_security_group" "weather_app_nsg" {
   name                = "${var.resource_group_name}-nsg"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
 
   security_rule {
     name                       = "SSH"
@@ -39,22 +54,10 @@ resource "azurerm_network_security_group" "weather_app_nsg" {
   }
 }
 
-resource "azurerm_public_ip" "main" {
-  name                = "${var.resource_group_name}-public-ip"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Dynamic"
-  sku                 = "Basic"
-  sku_tier            = "Regional"
-  ip_version          = "IPv4"
-  ddos_protection_mode = "VirtualNetworkInherited"
-  idle_timeout_in_minutes = 4
-}
-
 resource "azurerm_network_interface" "main" {
   name                = "${var.resource_group_name}-nic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
 
   ip_configuration {
     name                          = "internal"
@@ -68,14 +71,4 @@ resource "azurerm_network_interface" "main" {
 resource "azurerm_network_interface_security_group_association" "weather_app_nsg_assoc" {
   network_interface_id      = azurerm_network_interface.main.id
   network_security_group_id = azurerm_network_security_group.weather_app_nsg.id
-}
-
-resource "tls_private_key" "ssh" {
-  algorithm   = "RSA"
-  rsa_bits    = 4096
-  ecdsa_curve = "P224"
-}
-
-output "ssh_public_key" {
-  value = tls_private_key.ssh.public_key_openssh
 }
