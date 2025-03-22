7005498e4fd3441d70e8ae2ca113a9c47fa8d528
sebastianG <sebastian_gresala@yahoo.com>
2025-03-22 15:47:03 +0200

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,3 +1,5 @@
+# main.tf
+
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.resource_group_location
@@ -8,6 +10,8 @@ resource "azurerm_virtual_network" "main" {
   address_space       = ["10.0.0.0/16"]
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
+
+  depends_on = [azurerm_resource_group.main]
 }
 
 resource "azurerm_subnet" "main" {
@@ -15,6 +19,8 @@ resource "azurerm_subnet" "main" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.1.0/24"]
+
+  depends_on = [azurerm_virtual_network.main]
 }
 
 resource "azurerm_public_ip" "main" {
@@ -23,6 +29,8 @@ resource "azurerm_public_ip" "main" {
   resource_group_name = var.resource_group_name
   allocation_method   = "Dynamic"
   sku                 = "Basic"
+
+  depends_on = [azurerm_resource_group.main]
 }
 
 resource "azurerm_network_interface" "main" {
@@ -36,4 +44,6 @@ resource "azurerm_network_interface" "main" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.main.id
   }
+
+  depends_on = [azurerm_subnet.main, azurerm_public_ip.main]
 }
