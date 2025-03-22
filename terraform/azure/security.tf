ccd4cebe43932304f844b99b94c660a7af828e1a
sebastianG <sebastian_gresala@yahoo.com>
2025-03-22 15:36:40 +0200

    Update security.tf

--- terraform/azure/security.tf
+++ terraform/azure/security.tf
@@ -1,7 +1,7 @@
 resource "azurerm_network_security_group" "weather_app_nsg" {
-  name                = "weather-app-nsg"
-  location            = azurerm_resource_group.weather_app_rg.location
-  resource_group_name = azurerm_resource_group.weather_app_rg.name
+  location            = azurerm_resource_group.main.location     # ✅
+  resource_group_name = azurerm_resource_group.main.name         # ✅
+}
 
   security_rule {
     name                       = "SSH"
