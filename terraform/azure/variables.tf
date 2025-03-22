b013c8651f8d43c81acde36231ff7cf2bb1bdd0a
sebastianG <sebastian_gresala@yahoo.com>
2025-03-22 17:50:14 +0200

    Update variables.tf

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -41,11 +41,6 @@ variable "ssh_public_key" {
   type        = string
 }
 
-# -------------------------
-# outputs.tf
-# -------------------------
-output "ssh_public_key" {
-  value = tls_private_key.ssh.public_key_openssh
-}
+
 
 
