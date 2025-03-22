a6b26d5953a4ac35225175df487abbf51652f2ca
sebastianG <sebastian_gresala@yahoo.com>
2025-03-22 14:36:30 +0200

    Update provider.tf

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -1,23 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+    tls = {
+      source = "hashicorp/tls"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
 provider "azurerm" {
-  features = {}
+  features {}
 
   subscription_id = var.azure_subscription_id
   client_id       = var.azure_client_id
   client_secret   = var.azure_client_secret
   tenant_id       = var.azure_tenant_id
-}
-
-terraform {
-  required_version = ">= 1.3.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.24.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0.0"
-    }
-  }
 }
