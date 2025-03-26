c37c109421ab54018ba9aa5a019a138b6a70d892
sebastianG <sebastian_gresala@yahoo.com>
2025-03-26 18:26:12 +0200

    Update outputs.tf

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -1,14 +1,9 @@
+output "instance_id" {
+  value       = aws_instance.vm.id
+  description = "ID of the EC2 instance"
+}
+
 output "s3_bucket_arn" {
   value       = aws_s3_bucket.example.arn
   description = "ARN of the S3 bucket"
-}
-
-output "ec2_instance_id" {
-  value       = aws_instance.vm.id
-  description = "EC2 Instance ID"
-}
-
-output "ec2_public_ip" {
-  value       = aws_instance.vm.public_ip
-  description = "Public IP of the EC2 instance"
 }
