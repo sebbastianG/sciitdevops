81235da14c60f7dc4acb0025a077a8263a4780b6
sebastianG <sebastian_gresala@yahoo.com>
2025-03-26 17:42:28 +0200

    Update ec2_instance.tf

--- terraform/aws/ec2_instance.tf
+++ terraform/aws/ec2_instance.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "vm" {
-  ami                         = "ami-0c55b159cbfafe1f0" # ✅ Choose the correct AMI for your region
+  ami                         = "ami-0c55b159cbfafe1f0" # Update based on region
   instance_type               = "t2.micro"
-  key_name                    = var.key_name
   associate_public_ip_address = true
+  key_name                    = null # Or provide your key name
 
   tags = {
     Name = "${var.resource_group_name}-vm"
