02cc0b5f53fbfbb4b5f373fae02327ceac2ebc6f
sebastianG <sebastian_gresala@yahoo.com>
2025-03-26 17:22:02 +0200

    Update outputs.tf

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -1,11 +1,5 @@
-# terraform/aws/outputs.tf
-
-output "instance_id" {
-  description = "The ID of the EC2 instance"
-  value       = aws_instance.example.id
-}
-
-output "s3_bucket_arn" {
-  description = "The ARN of the S3 bucket"
-  value       = aws_s3_bucket.example.arn
+resource "aws_instance" "vm" {
+  ami           = "ami-0abcdef1234567890"
+  instance_type = "t2.micro"
+  ...
 }
