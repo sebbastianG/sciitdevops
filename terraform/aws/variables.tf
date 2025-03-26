c11887a490b0d807f3f7db902c9571a82c3c3175
sebastianG <sebastian_gresala@yahoo.com>
2025-03-26 16:16:35 +0200

    Create variables.tf

---
+++ terraform/aws/variables.tf
@@ -0,0 +1,27 @@
+# terraform/aws/variables.tf
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
