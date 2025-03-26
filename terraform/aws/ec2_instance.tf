aa440244fdcf7cf2adbafdf98eadeb6d3d10bb76
sebastianG <sebastian_gresala@yahoo.com>
2025-03-26 18:37:33 +0200

    Update ec2_instance.tf

--- terraform/aws/ec2_instance.tf
+++ terraform/aws/ec2_instance.tf
@@ -1,10 +1,23 @@
 resource "aws_instance" "vm" {
-  ami                         = "ami-0c55b159cbfafe1f0" # Update this to match your region
-  instance_type               = "t2.micro"
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.generated.key_name
   associate_public_ip_address = true
-  key_name                    = var.key_name
 
   tags = {
     Name = "${var.resource_group_name}-vm"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo Hello from Terraform"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = var.vm_admin_username
+      private_key = tls_private_key.ssh.private_key_pem
+      host        = self.public_ip
+    }
   }
 }
