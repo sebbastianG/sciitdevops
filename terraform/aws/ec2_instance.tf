b3f7dfffd01621774eab856469a11ec11352c618
sebastianG <sebastian_gresala@yahoo.com>
2025-03-26 17:32:02 +0200

    Create ec2_instance.tf

---
+++ terraform/aws/ec2_instance.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "vm" {
+  ami                         = "ami-0c55b159cbfafe1f0"
+  instance_type               = "t2.micro"
+  key_name                    = "your-key-name" # optional
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "${var.resource_group_name}-vm"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo Hello from Terraform"
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    user        = var.vm_admin_username
+    private_key = file("~/.ssh/id_rsa")
+    host        = self.public_ip
+  }
+}
