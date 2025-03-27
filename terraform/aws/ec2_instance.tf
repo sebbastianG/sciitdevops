6e435c9cf14ceb9b7bf25a318f644ac46f5a036a
sebastianG <sebastian_gresala@yahoo.com>
2025-03-27 13:44:28 +0200

    Update ec2_instance.tf

--- terraform/aws/ec2_instance.tf
+++ terraform/aws/ec2_instance.tf
@@ -1,27 +1,9 @@
-# -------------------------
-# ec2_instance.tf
-# -------------------------
-
-# Get the latest Ubuntu 22.04 LTS AMI from Canonical
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
+# terraform/aws/ec2_instance.tf
 
 resource "aws_instance" "vm" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.generated.key_name
+  key_name                    = var.key_name
   associate_public_ip_address = true
 
   tags = {
@@ -32,12 +14,12 @@ resource "aws_instance" "vm" {
     inline = [
       "echo Hello from Terraform"
     ]
+  }
 
-    connection {
-      type        = "ssh"
-      user        = var.vm_admin_username           # This will be set to "sebastian"
-      private_key = file(var.ssh_private_key_path)  # Path passed via tfvars or GitHub secret
-      host        = self.public_ip
-    }
+  connection {
+    type        = "ssh"
+    user        = var.vm_admin_username
+    private_key = file(var.ssh_private_key_path)
+    host        = self.public_ip
   }
 }
