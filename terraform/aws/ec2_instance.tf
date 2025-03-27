f137b7f087768331b59c2841fb14085f91d94c82
sebastianG <sebastian_gresala@yahoo.com>
2025-03-27 18:15:24 +0200

    Update ec2_instance.tf

--- terraform/aws/ec2_instance.tf
+++ terraform/aws/ec2_instance.tf
@@ -4,14 +4,30 @@ resource "aws_instance" "vm" {
   key_name                    = var.key_name
   associate_public_ip_address = true
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo 'sebastian:Seb123!@#' | chpasswd
-              apt-get update -y
-              apt-get install -y nginx
-              EOF
-
   tags = {
     Name = "${var.resource_group_name}-vm"
+  }
+
+  user_data = <<EOF
+#!/bin/bash
+sudo useradd -m -s /bin/bash sebastian
+echo "sebastian:Seb123!@#" | sudo chpasswd
+sudo usermod -aG sudo sebastian
+EOF
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo VM ready",
+      "sudo apt-get update -y",
+      "sudo apt-get install -y nginx"
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = "sebastian"
+      host     = self.public_ip
+      password = "Seb123!@#"
+      timeout  = "2m"
+    }
   }
 }
