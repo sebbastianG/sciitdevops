b31cd4bd59975b65907eef5834529dd4380755ba
sebastianG <sebastian_gresala@yahoo.com>
2025-03-27 18:08:03 +0200

    Update ec2_instance.tf

--- terraform/aws/ec2_instance.tf
+++ terraform/aws/ec2_instance.tf
@@ -1,27 +1,17 @@
-# terraform/aws/ec2_instance.tf
-
 resource "aws_instance" "vm" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
   key_name                    = var.key_name
   associate_public_ip_address = true
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo 'sebastian:Seb123!@#' | chpasswd
+              apt-get update -y
+              apt-get install -y nginx
+              EOF
+
   tags = {
     Name = "${var.resource_group_name}-vm"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update -y",
-      "sudo apt-get install -y nginx"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = var.vm_admin_username
-      host        = self.public_ip
-      private_key = file("~/.ssh/sebastian.pem")
-      timeout     = "2m"
-    }
   }
 }
