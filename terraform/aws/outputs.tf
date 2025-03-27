57a20dfdacd7cc46eae90b607611910a970f9ea0
sebastianG <sebastian_gresala@yahoo.com>
2025-03-27 14:50:00 +0200

    Update outputs.tf

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -1,9 +1,7 @@
 output "instance_id" {
-  value       = aws_instance.vm.id
-  description = "ID of the EC2 instance"
+  value = aws_instance.vm.id
 }
 
 output "s3_bucket_arn" {
-  value       = aws_s3_bucket.example.arn
-  description = "ARN of the S3 bucket"
+  value = aws_s3_bucket.example.arn
 }
