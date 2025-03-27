59f42cf3327acb301a1f92ea3e28fa0f140f1d74
sebastianG <sebastian_gresala@yahoo.com>
2025-03-27 18:08:20 +0200

    Update outputs.tf

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -1,17 +1,7 @@
-# terraform/aws/outputs.tf
-
 output "instance_id" {
   value = aws_instance.vm.id
 }
 
 output "s3_bucket_arn" {
   value = aws_s3_bucket.example.arn
-}
-
-# terraform/aws/providers.tf
-
-provider "aws" {
-  region     = var.aws_default_region
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
 }
