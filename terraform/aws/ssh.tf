0b359fca462ed9a8fa4a638d25132f376f5abfb6
sebastianG <sebastian_gresala@yahoo.com>
2025-03-27 14:07:37 +0200

    Update ssh.tf

--- terraform/aws/ssh.tf
+++ terraform/aws/ssh.tf
@@ -1,13 +1,4 @@
-resource "tls_private_key" "ssh" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "random_id" "suffix" {
-  byte_length = 4
-}
-
 resource "aws_key_pair" "generated" {
-  key_name   = "generated-${random_id.suffix.hex}"
-  public_key = tls_private_key.ssh.public_key_openssh
+  key_name   = var.key_name
+  public_key = var.ssh_public_key
 }
