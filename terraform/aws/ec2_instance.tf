3b8574374c4a4a3039f73fd071d70c74405af67d
sebastianG <sebastian_gresala@yahoo.com>
2025-03-27 19:25:54 +0200

    Update ec2_instance.tf

--- terraform/aws/ec2_instance.tf
+++ terraform/aws/ec2_instance.tf
@@ -1,34 +1,32 @@
 resource "aws_instance" "vm" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
-  key_name                    = var.key_name
   associate_public_ip_address = true
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo 'sebastian:Seb123!@#' | chpasswd
-              sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-              systemctl restart sshd
-              EOF
-
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "${var.resource_group_name}-vm"
   }
 
+  user_data = <<-EOF
+              #!/bin/bash
+              useradd -m -s /bin/bash ${var.vm_admin_username}
+              echo "${var.vm_admin_username}:${var.vm_admin_password}" | chpasswd
+              echo "${var.vm_admin_username} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+              sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+              systemctl restart ssh
+              EOF
+
   provisioner "remote-exec" {
     inline = [
-      "sudo apt-get update -y",
-      "sudo apt-get install -y nginx"
+      "echo 'Provisioned successfully!'"
     ]
 
     connection {
       type     = "ssh"
       user     = var.vm_admin_username
-      password = "Seb123!@#"
+      password = var.vm_admin_password
       host     = self.public_ip
-      timeout  = "3m"
+      timeout  = "2m"
     }
   }
 }
