e8cf6789f0ef0f3c1d4185b19482da7f8253fc70
sebastianG <sebastian_gresala@yahoo.com>
2025-03-27 17:55:34 +0200

    Update s3_instance.tf

--- terraform/aws/s3_instance.tf
+++ terraform/aws/s3_instance.tf
@@ -1,8 +1,16 @@
+# terraform/aws/s3_instance.tf
+
+resource "random_string" "rand" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
 resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name
+  bucket = "${var.bucket_name}-${random_string.rand.result}"
 
   tags = {
-    Name        = "my-terraform-bucket-sebi123-xyz"
+    Name        = "my-terraform-bucket-${random_string.rand.result}"
     Environment = "Dev"
   }
 }
