84b147480c21b632d3186a8d39963e280bdd735e
sebastianG <sebastian_gresala@yahoo.com>
2025-03-27 18:07:33 +0200

    Update ami.tf

--- terraform/aws/ami.tf
+++ terraform/aws/ami.tf
@@ -1,8 +1,6 @@
-# terraform/aws/ami.tf
-
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["099720109477"]
+  owners      = ["099720109477"] # Canonical
 
   filter {
     name   = "name"
