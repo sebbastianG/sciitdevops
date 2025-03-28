a5c7130c954b485da2b337f835d62f2de557d238
sebastianG <sebastian_gresala@yahoo.com>
2025-03-28 11:18:10 +0200

    Update s3_instance.tf

--- terraform/aws/s3_instance.tf
+++ terraform/aws/s3_instance.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "jefrijeronimo" {
-  bucket = var.bucket_name
+  bucket = "${var.bucket_name}-${replace(timestamp(), ":", "-")}"
 
   tags = {
     Name        = var.bucket_name
