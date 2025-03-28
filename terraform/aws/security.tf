555c367a5bf6f0644ff494e5f73ee61cb3bc128d
sebastianG <sebastian_gresala@yahoo.com>
2025-03-28 10:10:54 +0200

    Update security.tf

--- terraform/aws/security.tf
+++ terraform/aws/security.tf
@@ -16,6 +16,10 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "allow_ssh"
+  }
 }
 
 data "aws_vpc" "default" {
