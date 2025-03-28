001aad1c1a23dd1efd39427b4afb38c49ab3d6e7
sebastianG <sebastian_gresala@yahoo.com>
2025-03-28 10:12:39 +0200

    Update ssh.tf

--- terraform/aws/ssh.tf
+++ terraform/aws/ssh.tf
@@ -1,4 +1,21 @@
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.generated.private_key_pem
+  filename = "${path.module}/generated-key.pem"
+  file_permission = "0600"
+}
+
+resource "local_file" "public_key" {
+  content  = tls_private_key.generated.public_key_openssh
+  filename = "${path.module}/generated-key.pub"
+  file_permission = "0644"
+}
+
 resource "aws_key_pair" "generated" {
   key_name   = var.key_name
-  public_key = var.ssh_public_key
+  public_key = tls_private_key.generated.public_key_openssh
 }
