e5335109cd33b3deeaba2b7243e23a650ff3f8a1
sebastianG <sebastian_gresala@yahoo.com>
2025-03-29 12:05:09 +0200

    Update ec2_instance.tf

--- terraform/aws/ec2_instance.tf
+++ terraform/aws/ec2_instance.tf
@@ -1,35 +1,25 @@
+
 resource "aws_instance" "vm" {
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
-  key_name                    = aws_key_pair.generated.key_name
-
-  tags = {
-    Name = "${var.resource_group_name}-vm"
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              useradd -m -s /bin/bash ${var.vm_admin_username}
-              echo "${var.vm_admin_username}:${var.vm_admin_password}" | chpasswd
-              echo "${var.vm_admin_username} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-              sed -i 's/^#\\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
-              systemctl restart ssh
-              EOF
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.ec2_key.key_name
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
 
   provisioner "remote-exec" {
     inline = [
-      "sleep 30",
-      "echo 'Provisioned successfully!'"
+      "echo Connected!"
     ]
 
     connection {
-      type     = "ssh"
-      user     = var.vm_admin_username
-      password = var.vm_admin_password
-      host     = self.public_ip
-      timeout  = "2m"
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = tls_private_key.ec2_key.private_key_pem
+      host        = self.public_ip
     }
+  }
+
+  tags = {
+    Name = "TerraformInstance"
   }
 }
