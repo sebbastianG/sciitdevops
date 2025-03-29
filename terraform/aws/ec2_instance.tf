1844c1767561b3f31f4a8d7debbd1d420c3b625e
sebastianG <sebastian_gresala@yahoo.com>
2025-03-29 13:50:43 +0200

    Update ec2_instance.tf

--- terraform/aws/ec2_instance.tf
+++ terraform/aws/ec2_instance.tf
@@ -1,25 +1,50 @@
 
-resource "aws_instance" "vm" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.ec2_key.key_name
-  subnet_id              = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+resource "aws_instance" "k3s" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.ec2_key.key_name
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
 
   provisioner "remote-exec" {
     inline = [
-      "echo Connected!"
+      "echo Connected to k3s node"
     ]
 
     connection {
       type        = "ssh"
-      user        = "ec2-user"
+      user        = var.vm_admin_username
       private_key = tls_private_key.ec2_key.private_key_pem
       host        = self.public_ip
     }
   }
 
   tags = {
-    Name = "TerraformInstance"
+    Name = "k3s"
+  }
+}
+
+resource "aws_instance" "monitoring" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.ec2_key.key_name
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo Connected to monitoring node"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = var.vm_admin_username
+      private_key = tls_private_key.ec2_key.private_key_pem
+      host        = self.public_ip
+    }
+  }
+
+  tags = {
+    Name = "monitoring"
   }
 }
