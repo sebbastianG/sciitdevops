cce24ebce8aaa2a51b5415004b2501fb229218c4
sebastianG <sebastian_gresala@yahoo.com>
2025-03-29 13:57:36 +0200

    Update outputs.tf

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -1,13 +1,20 @@
-
-output "instance_public_ip" {
-  value = aws_instance.vm.public_ip
+output "k3s_public_ip" {
+  description = "Public IP of the k3s instance"
+  value       = aws_instance.k3s.public_ip
 }
 
-output "private_key_pem" {
-  value     = tls_private_key.ec2_key.private_key_pem
-  sensitive = true
+output "monitoring_public_ip" {
+  description = "Public IP of the monitoring instance"
+  value       = aws_instance.monitoring.public_ip
 }
 
 output "s3_bucket_name" {
-  value = aws_s3_bucket.jefrijeronimo.bucket
+  description = "Name of the created S3 bucket"
+  value       = aws_s3_bucket.jefrijeronimo.bucket
+}
+
+output "private_key_pem" {
+  description = "Private key for SSH access"
+  value       = tls_private_key.ec2_key.private_key_pem
+  sensitive   = true
 }
