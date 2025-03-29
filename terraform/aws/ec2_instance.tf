ca79b8f213814141f23b08e33d16a338fcbe5276
sebastianG <sebastian_gresala@yahoo.com>
2025-03-29 13:15:15 +0200

    Update ec2_instance.tf

--- terraform/aws/ec2_instance.tf
+++ terraform/aws/ec2_instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "vm" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = aws_key_pair.ec2_key.key_name
-  subnet_id              = var.subnet_id
+  subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   provisioner "remote-exec" {
