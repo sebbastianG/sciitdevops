ae23078624fe826c63bc105f310d842d3d893ef4
sebastianG <sebastian_gresala@yahoo.com>
2025-03-29 12:05:19 +0200

    Update outputs.tf

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -1,7 +1,13 @@
-output "instance_id" {
-  value = aws_instance.vm.id
+
+output "instance_public_ip" {
+  value = aws_instance.vm.public_ip
 }
 
-output "s3_bucket_arn" {
-  value = aws_s3_bucket.jefrijeronimo.arn
+output "private_key_pem" {
+  value     = tls_private_key.ec2_key.private_key_pem
+  sensitive = true
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.jefrijeronimo.bucket
 }
